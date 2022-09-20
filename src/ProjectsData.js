0027873cb478bdffcb0976e8033e10a6dd3fe05f
Arthur Moreira <arthurmcs96@gmail.com>
2022-09-19 22:40:07 -0300

    remove o trybe wallet

--- src/ProjectsData.js
+++ src/ProjectsData.js
@@ -6,13 +6,6 @@ const projectsData = [
     github: 'https://github.com/ArthurMCS/myTrybetunes',
     description: 'Com o objetivo de simular um site de musicas, este projeto foi feito com React e algumas bibliotecas para a estilização, consumindo a API do Itunes. ',
   },
-  {
-    id: 2,
-    name: 'TrybeWallet',
-    link: ' https://arthurmcs.github.io/my-trybewallet/',
-    github: 'https://github.com/ArthurMCS/my-trybewallet',
-    description: 'Este projeto é uma carteira de controle de gastos com conversor de moedas em tempo real, usando o Redux como ferramenta de manipulação de estado.',
-  },
 ];
 
 export default projectsData;
